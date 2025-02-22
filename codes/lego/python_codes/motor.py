254ce69bf6c92f9d2e25584f26ddce0933e45e4a
Luis Ballado <alberto.balladoar@gmail.com>
2025-02-22 04:21:51 -0600

    lego

---
+++ codes/lego/python_codes/motor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""NXT-Python tutorial: turn a motor."""
+import nxt.locator
+import nxt.motor
+
+with nxt.locator.find() as b:
+    print(b.get_device_info()[0:2])
+    # Get the motor connected to the port A.
+    mymotor = b.get_motor(nxt.motor.Port.A)
+    # Full circle in one direction.
+    mymotor.turn(25, 360)
+    # Full circle in the opposite direction.
+    mymotor.turn(-25, 360)
+    
